2374a623ab5bafcf12ef113ca80fa0f917f52f58
Taisir Hassan <taisirisawesome@gmail.com>
2021-08-13 16:42:41 -0400

    more changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <DarkMode />
       <Technologies />
       <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+function Footer () {
+  return (
+      <div class = "footer-container">
+
+
+</div>
+  )  
+}
+export default Footer;
