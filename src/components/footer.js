18e32e145f416376a0ff51526b9e0c518f0933b3
Taisir <taisirisawesome@gmail.com>
2021-08-20 17:19:14 -0400

    changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,11 @@
+import FadeIn from "react-fade-in/lib/FadeIn";
 function Footer() {
   return (
-    <div class="footer-container">
-      <h5> Taisir Hassan</h5>
-    </div>
+    <FadeIn>
+      <div class="footer-container">
+        <h5> Taisir Hassan</h5>
+      </div>
+    </FadeIn>
   );
 }
 export default Footer;
--- src/components/name-container.js
+++ src/components/name-container.js
@@ -1,26 +1,30 @@
+import FadeIn from "react-fade-in/lib/FadeIn";
+
 function Namecontainer() {
   return (
-    <div class="name-container" id="namer-container">
-      <h3 class="name-header">
-        My Name is <i>Taisir Hassan</i>
-        <p>
-          I am an Aspiring Computer Engineer currently a student at the
-          University of Waterloo!
-        </p>
-      </h3>
-      <div class="buttons">
-        <a href="mailto:t28hassa@uwaterloo.ca" class="email">
-          {" "}
-          Email Me{" "}
-        </a>
-        <a
-          href="https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:62b0da08-1e97-4ba1-a97e-6a630b329e27"
-          class="Resume"
-        >
-          Resume{" "}
-        </a>
+    <FadeIn>
+      <div class="name-container" id="namer-container">
+        <h3 class="name-header">
+          My Name is <i>Taisir Hassan</i>
+          <p>
+            I am an Aspiring Computer Engineer currently a student at the
+            University of Waterloo!
+          </p>
+        </h3>
+        <div class="buttons">
+          <a href="mailto:t28hassa@uwaterloo.ca" class="email">
+            {" "}
+            Email Me{" "}
+          </a>
+          <a
+            href="https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:62b0da08-1e97-4ba1-a97e-6a630b329e27"
+            class="Resume"
+          >
+            Resume{" "}
+          </a>
+        </div>
       </div>
-    </div>
+    </FadeIn>
   );
 }
 export default Namecontainer;
